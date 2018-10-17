daab5ff846061c2d5e080f2ce5c23f114a57087d
WilliamPotvinPageau <w.i.l.l.i.a.m.p.p@hotmail.com>
2018-10-17 11:13:36 -0400

    ExerciceJs2 et ExerciceJS3

--- Exercices JS/Exercice2/JS/traitement.js
+++ Exercices JS/Exercice2/JS/traitement.js
@@ -1,5 +1,10 @@
 function btnCalculer_onclick()
-{ 
- 	
+{
+    var MoyKmMois, TotKM;
+
+    MoyKmMois=parseFloat(document.getElementById("txtMoyKm").value);
+    TotKM=MoyKmMois*12*5;
+    console.log("Le cycliste fera "+TotKM+"Km dans les 5 prochaines années");
+
 
 }
--- Exercices JS/Exercice3/JS/traitement.js
+++ Exercices JS/Exercice3/JS/traitement.js
@@ -1,5 +1,9 @@
 function btnCalculer_onclick()
 { 
- 	
+ 	var ArgentCAN, TauxChangeEuro, Euro;
 
+    ArgentCAN=parseFloat(document.getElementById("txtArgntCad").value);
+    TauxChangeEuro=parseFloat(document.getElementById("txtTxChnge").value);
+    Euro=ArgentCAN*TauxChangeEuro;
+    console.log("Cela fait "+Euro+" Euros")
 }
