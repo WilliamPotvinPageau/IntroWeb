9c2b665648588587fade2f52f3880f57e49136c3
WilliamPotvinPageau <w.i.l.l.i.a.m.p.p@hotmail.com>
2018-10-19 10:34:54 -0400

    ExerciceJs4 Fait

--- Exercices JS/Exercice4/JS/traitement.js
+++ Exercices JS/Exercice4/JS/traitement.js
@@ -1,5 +1,13 @@
 function btnCalculer_onclick()
-{ 
- 	
+{
+    var Destination, Distance, Consommation, PrixLitre, Cout;
+
+    Destination=(document.getElementById("txtDestination").value);
+    Distance=parseFloat(document.getElementById("txtDistance").value);
+    Consommation=parseFloat(document.getElementById("txtConsommation").value);
+    PrixLitre=parseFloat(document.getElementById("txtPrixLitre").value);
+    Cout=((Distance/100)*Consommation)*PrixLitre;
+
+    console.log("Votre voyage vers "+Destination+" vous coutera "+Cout+"$");
 
 }
