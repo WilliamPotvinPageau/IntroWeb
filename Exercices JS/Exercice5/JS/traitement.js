0b01a5c0e280c5532d7fefb2cb304cdf6c5f779d
WilliamPotvinPageau <w.i.l.l.i.a.m.p.p@hotmail.com>
2018-10-19 10:43:03 -0400

    ExerciceJs5 Fait

--- Exercices JS/Exercice5/JS/traitement.js
+++ Exercices JS/Exercice5/JS/traitement.js
@@ -1,5 +1,13 @@
 function btnCalculer_onclick()
-{ 
- 	
+{
+    var Revenus, Loyer, Nourriture, Deplacement, Reste;
+
+    Revenus=(document.getElementById("txtRevenus").value);
+    Loyer=parseFloat(document.getElementById("txtLoyer").value);
+    Nourriture=parseFloat(document.getElementById("txtNourriture").value);
+    Deplacement=parseFloat(document.getElementById("txtDeplacement").value);
+    Reste=12*(Revenus-Loyer-Nourriture-Deplacement)/52;
+
+    console.log("Il vous reste "+Reste+"$ par semaines");
 
 }
