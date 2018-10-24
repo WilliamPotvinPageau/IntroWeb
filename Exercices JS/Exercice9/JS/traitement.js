97f265a946046e47a6b4e35f74952ce4055a8b4d
WilliamPotvinPageau <w.i.l.l.i.a.m.p.p@hotmail.com>
2018-10-24 09:43:16 -0400

    ExesciceJs9 terminé

--- Exercices JS/Exercice9/JS/traitement.js
+++ Exercices JS/Exercice9/JS/traitement.js
@@ -1,5 +1,23 @@
 function btnCalculer_onclick()
 { 
- 	
+ 	var NbBilletEnf, NbBilletEtu, NbBillet65, NbBilletAdlt, Rabais,Cout, Total;
 
+    NbBilletEnf=parseFloat(document.getElementById("txtEnf").value);
+    NbBilletEtu=parseFloat(document.getElementById("txtEtu").value);
+    NbBillet65=parseFloat(document.getElementById("txt65+").value);
+    NbBilletAdlt=parseFloat(document.getElementById("txtAdlt").value);
+
+    if(NbBilletEtu+NbBillet65+NbBilletAdlt > 5)
+    {
+ 	     Rabais=0.10
+    }
+    else
+    {
+        Rabais=0
+    }
+
+    Cout=((NbBilletEtu*12)+(NbBillet65*15)+(NbBilletAdlt*20));
+    Total = Cout-(Cout*Rabais)
+
+ 	console.log("Le coût total de vos billets est de "+Total+"$")
 }
