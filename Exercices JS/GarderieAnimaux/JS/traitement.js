f03f09a86c1f468586d74d74e361cadc0ad9d9ec
WilliamPotvinPageau <w.i.l.l.i.a.m.p.p@hotmail.com>
2018-10-24 11:01:07 -0400

    GarderieAnimaux Terminé

--- Exercices JS/GarderieAnimaux/JS/traitement.js
+++ Exercices JS/GarderieAnimaux/JS/traitement.js
@@ -1,5 +1,35 @@
 function btnCalculer_onclick()
-{ 
- 	
+{
+    var Veterinaire, NbJrs, Service,ServMsg, Animal, Tarif, Cout, Total;
+
+    Veterinaire=(document.getElementById("lstVeterinaire").value);
+    NbJrs=parseFloat(document.getElementById("txtNbreJours").value);
+
+    if((document.getElementById("chkServ").checked))
+    {
+        Service=5;
+        ServMsg="avec";
+    }
+    else
+    {
+        Service=0;
+        ServMsg="sans";
+    }
+
+    if((document.getElementById("radChien").checked))
+    {
+        Tarif=18.50;
+        Animal="chien";
+    }
+    else
+    {
+        Tarif=16.95;
+        Animal="chat";
+
+    }
+
+    Cout=(Tarif*NbJrs)+Service;
+    Total=Cout+(Cout*0.14975);
+    console.log("Vétérinaire responsable est "+Veterinaire+". Le montant pour la garde de votre "+Animal+" est de "+Total+" pour "+NbJrs+" jours, "+ServMsg+" service de toilettage.")
 
 }
