c931308e334987fd2a70d20080db9602d1915068
WilliamPotvinPageau <w.i.l.l.i.a.m.p.p@hotmail.com>
2018-11-02 09:49:51 -0400

    Exercices Choisir parmi Terminé

--- Exercices JS/Exercices  - Choisir Parmi/JS/traitement13.js
+++ Exercices JS/Exercices  - Choisir Parmi/JS/traitement13.js
@@ -1,5 +1,28 @@
 function btnCalculer_onclick()
 { 
- 	
+ 	var Num, Jour;
+
+ 	Num=parseInt(document.getElementById("txtNumSem").value);
+ 	switch(Num)
+    {
+        case 1: Jour="Lundi";
+            break;
+        case 2: Jour="Mardi";
+            break;
+        case 3: Jour="Mercredi";
+            break;
+        case 4: Jour="Jeudi";
+            break;
+        case 5: Jour="Vendredi";
+            break;
+        case 6: Jour="Samedi";
+            break;
+        case 7: Jour="Dimanche";
+            break;
+        default: Jour="Erreur";
+            break;
+    }
+
+    document.getElementById("lblMessage").innerHTML=Jour;
 
 }
