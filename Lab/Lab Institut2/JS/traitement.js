0d3fe062fc07e5b60edf3443cc991ee479b9a154
WilliamPotvinPageau <w.i.l.l.i.a.m.p.p@hotmail.com>
2018-11-30 10:28:51 -0500

    lab institut terminé

--- Lab/Lab Institut2/JS/traitement.js
+++ Lab/Lab Institut2/JS/traitement.js
@@ -1,29 +1,56 @@
-function frmMembre_onclick()
+function frmMembre_onsubmit()
 {
+    var valide=false;
+
     if(valideChamps()===true)
     {
+        document.getElementById("lblMessageErreur").innerHTML="";
         if (valideFormat()===true)
         {
-
+            if(confirm("Souhaitez-vous payer le prix d'inscription de "+calculTarif()+"$ afin de finaliser votre inscription?")===true)
+            {
+                valide=true;
+            }
+        }
+        else
+        {
+            document.getElementById("lblMessageErreur").innerHTML="Les champs ne sont pas tous valide.";
         }
     }
     else
     {
-        document.getElementById("lblMessageErreur").innerHTML="Tous les champs ayant une étoile sont obligatoires."
+        document.getElementById("lblMessageErreur").innerHTML="Tous les champs ayant une étoile sont obligatoires.";
     }
  
+    return valide;
+}
+function calculTarif()
+{
+    var tarif, Categorie=document.getElementById("type").value;
 
+    switch (Categorie)
+    {
+        case "adulte": tarif=90;
+        break;
+
+        case "étudiant": tarif=60;
+        break;
+
+        case "retraité": tarif=80;
+        break;
+    }
+    return tarif;
 }
 function valideChamps()
 {
     var TabValide=document.getElementsByName("txt");
     var i, valide=false;
 
-    for (i=0;i<4;i++)
+    for (i=0;i<5;i++)
     {
         if (valideExiste(TabValide[i])===true)
         {
-            valide=true
+            valide=true;
         }
     }
     return valide
@@ -32,90 +59,92 @@ function valideChamps()
 function valideExiste(Txt)
 {
     var valide=true;
-    if (txt==="")
+    if (Txt.value==="")
     {
         valide=false;
-        txt.style.borderColor="red";
+        Txt.style.borderColor="red";
     }
     else
     {
-        txt.style.borderColor="";
+        Txt.style.borderColor="";
     }
     return valide;
 }
 function valideFormat()
 {
     var valide=false;
+
     if(valideNom(document.getElementById("txtNom").value)===true)
     {
         valide=true;
-        document.getElementById("txtNom").style.borderColor=""
+        document.getElementById("txtNom").style.borderColor="";
     }
     else
     {
-        document.getElementById("txtNom").style.borderColor="red"
+        document.getElementById("txtNom").style.borderColor="red";
     }
     if(valideNom(document.getElementById("txtPrenom").value)===true)
     {
         valide=true;
-        document.getElementById("txtPrenom").style.borderColor=""
+        document.getElementById("txtPrenom").style.borderColor="";
     }
     else
     {
-        document.getElementById("txtPrenom").style.borderColor="red"
+        document.getElementById("txtPrenom").style.borderColor="red";
     }
     if(valideNom(document.getElementById("txtVille").value)===true)
     {
         valide=true;
-        document.getElementById("txtVille").style.borderColor=""
+        document.getElementById("txtVille").style.borderColor="";
     }
     else
     {
-        document.getElementById("txtVille").style.borderColor="red"
+        document.getElementById("txtVille").style.borderColor="red";
     }
     if(valideCodePostal(document.getElementById("txtCodePostal").value)===true)
     {
         valide=true;
-        document.getElementById("txtCodePostal").style.borderColor=""
+        document.getElementById("txtCodePostal").style.borderColor="";
     }
     else
     {
-        document.getElementById("txtCodePostal").style.borderColor="red"
+        document.getElementById("txtCodePostal").style.borderColor="red";
     }
 
     if(valideTel(document.getElementById("txtTel").value)===true)
     {
         valide=true;
-        document.getElementById("txtTel").style.borderColor="red"
+        document.getElementById("txtTel").style.borderColor="";
     }
     else
     {
-        document.getElementById("txtTel").style.borderColor=""
+        document.getElementById("txtTel").style.borderColor="red";
     }
 
     if(valideCodePerm(document.getElementById("txtCodePerm").value)===true)
     {
         valide=true;
-        document.getElementById("txtCodePerm").style.borderColor=""
+        document.getElementById("txtCodePerm").style.borderColor="";
     }
     else
     {
-        document.getElementById("txtCodePerm").style.borderColor="red"
+        document.getElementById("txtCodePerm").style.borderColor="red";
     }
+    return valide;
 }
 function valideNom(Nom)
 {
-    return /[]/.test(Nom);
+    return /^[A-Z][a-z]+(-[A-Z][a-z]+)?$/.test(Nom);
 }
 function valideCodePostal(Code)
 {
-    return /[]/.test(Code);
+    return /^[A-z]\d[A-z] \d[A-z]\d$/.test(Code);
 }
 function valideTel(Tel)
 {
-    return /[]/.test(Tel);
+    return /^(\d{3}-\d{3}-\d{4}|\(\d{3}\) \d{3}-\d{4})$/.test(Tel);
 }
 function valideCodePerm(Code)
 {
-    return /[]/.test(Code);
+    return /^[A-z]{4}\d{8}$/.test(Code);
 }
